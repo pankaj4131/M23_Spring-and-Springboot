7ded1b1e33c51bbe34a334a6f1d85bd162861bec
pankaj4131 <pankajmanjare516@gmail.com>
2022-10-04 11:36:31 +0530

    First Commit

---
+++ SpringStarterProject/src/main/java/com/example/demo/SpringStarterProjectApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+@ComponentScan
+@Configuration
+@EnableAutoConfiguration
+@SpringBootApplication
+public class SpringStarterProjectApplication {
+
+	public static void main(String[] args) {
+		
+		ConfigurableApplicationContext c=SpringApplication.run(SpringStarterProjectApplication.class, args);
+		Student s=c.getBean(Student.class);
+		//Student s=new Student();
+		s.display();
+	}
+
+}
